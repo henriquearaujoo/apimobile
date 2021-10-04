d5168b852598057053068d60be0411ff75df410e
henriquearaujo <henriquearaujoo@gmail.com>
2021-10-04 17:08:21 -0300

    update to NET 5.0

--- src/Ailos.Http/SOAClient.cs
+++ src/Ailos.Http/SOAClient.cs
@@ -5,22 +5,22 @@ namespace Ailos.Http
 {
     public class SOAClient : IClient
     {
-        public Task<T> Delete<T>(string method, string url, object body, Dictionary<string, object> queryParams, Dictionary<string, object> pathParams, Dictionary<string, object> headerParams) where T : new()
+        public Task<T> Delete<T>(string url, object body, Dictionary<string, object> queryParams, Dictionary<string, object> pathParams, Dictionary<string, object> headerParams) where T : new()
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<T> Get<T>(string method, string url, Dictionary<string, object> queryParams, Dictionary<string, object> pathParams, Dictionary<string, object> headerParams) where T : new()
+        public Task<T> Get<T>(string url, Dictionary<string, object> queryParams, Dictionary<string, object> pathParams, Dictionary<string, object> headerParams) where T : new()
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<T> Post<T>(string method, string url, object body, Dictionary<string, object> queryParams, Dictionary<string, object> pathParams, Dictionary<string, object> headerParams) where T : new()
+        public Task<T> Post<T>(string url, object body, Dictionary<string, object> queryParams, Dictionary<string, object> pathParams, Dictionary<string, object> headerParams) where T : new()
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<T> Put<T>(string method, string url, object body, Dictionary<string, object> queryParams, Dictionary<string, object> pathParams, Dictionary<string, object> headerParams) where T : new()
+        public Task<T> Put<T>(string url, object body, Dictionary<string, object> queryParams, Dictionary<string, object> pathParams, Dictionary<string, object> headerParams) where T : new()
         {
             throw new System.NotImplementedException();
         }
