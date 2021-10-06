7bde033538dd5802b16b6723e46f7d8515e47f29
snyland <sandronyland98@gmail.com>
2021-10-05 22:26:37 -0300

    Adicionando API controller "keys"

---
+++ src/Ailos.ApiMobile.API/Controllers/v1/Pix/KeysController.cs
@@ -0,0 +1,25 @@
+﻿using Ailos.Pix.DTO.Key;
+using Ailos.Pix.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Ailos.ApiMobile.API.Controllers.v1.Pix
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KeysController : ControllerBase
+    {
+        private readonly IKeyService _keyService;
+
+        public KeysController(IKeyService keyService)
+        {
+            _keyService = keyService;
+        }
+
+        [HttpPost]
+        public async Task<NewKeyResponse> AddKey(NewKeyRequest newKeyRequest)
+        {
+            return await _keyService.AddKey(newKeyRequest);
+        }
+    }
+}
